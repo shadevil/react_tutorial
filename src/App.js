b55326b0e3b80d1fc542be981d2065843dbb9880
shadevil <linkinfan@bk.ru>
2022-06-06 12:14:30 +0500

    core functionality

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useMemo, useState } from "react";
+import PostFilter from "./components/PostFilter";
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+import "./styles/App.css";
+
+function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "aJavascript 1", body: "eDescription" },
+    { id: 2, title: "zJavascript 2", body: "1Description" },
+    { id: 3, title: "vJavascript 3", body: "pDescription" },
+    { id: 4, title: "iJavascript 4", body: "]Description" },
+  ]);
+
+  const [filter, setFilter] = useState({sort:'', query:''});
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+  const sortedPosts = useMemo(() => {
+    console.log('отработала сортировка');
+    if(filter.sort){
+      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
+    }   
+    return posts;
+  }, [filter.sort, posts])
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
+  },[filter.query, sortedPosts])
+  //Get post from child component
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  return (
+    <div className="App">
+      <PostForm create={createPost} />
+      <hr style={{margin: '15px 0'}}/>
+      <PostFilter 
+        filter={filter}
+        setFilter={setFilter}
+      />
+      {sortedAndSearchedPosts.length
+      ? 
+        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про js" />
+       : 
+        <h1 style={{ textAlign: "center" }}>
+          Посты не найдены
+        </h1>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PostItem from "./PostItem";
+const PostList = ({posts, title, remove}) => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>{title}</h1>
+      {posts.map((post, index) => (
+        <PostItem remove={remove} number={index + 1} key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
