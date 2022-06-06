aac7734c9ca0f0e23861e60979fa1f9914f2ef94
shadevil <linkinfan@bk.ru>
2022-06-06 16:15:18 +0500

    clear code, add get posts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React, { useMemo, useState } from "react";
+import { usePosts } from "./components/hooks/usePosts";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
@@ -7,35 +9,21 @@ import MyModal from "./components/UI/MyModal/MyModal";
 import "./styles/App.css";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "aJavascript 1", body: "eDescription" },
-    { id: 2, title: "zJavascript 2", body: "1Description" },
-    { id: 3, title: "vJavascript 3", body: "pDescription" },
-    { id: 4, title: "iJavascript 4", body: "]Description" },
-  ]);
+  const [posts, setPosts] = useState([ ]);
 
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort,filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
-  const sortedPosts = useMemo(() => {
-    console.log("отработала сортировка");
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
 
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    );
-  }, [filter.query, sortedPosts]);
+  async function fetchPosts(){
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    setPosts(response.data);
+  }
   //Get post from child component
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -43,6 +31,7 @@ function App() {
 
   return (
     <div className="App">
+      <MyButton style={{marginRight: 10}} onClick={fetchPosts}>GET POSTS</MyButton>
       <MyButton style={{marginTop: 20}} onClick={() => setModal(true)}>
         Создать пользователя
       </MyButton>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { CSSTransition } from "react-transition-group";
+import { TransitionGroup } from "react-transition-group";
 import PostItem from "./PostItem";
 const PostList = ({posts, title, remove}) => {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>{title}</h1>
+      <TransitionGroup>
       {posts.map((post, index) => (
-        <PostItem remove={remove} number={index + 1} key={post.id} post={post} />
+        <CSSTransition
+          key={post.id}
+          timeout={500}
+          classNames="post">
+        <PostItem 
+          remove={remove} 
+          number={index + 1} 
+          key={post.id} 
+          post={post} />
+        </CSSTransition>
       ))}
+      </TransitionGroup>
     </div>
   );
 };
