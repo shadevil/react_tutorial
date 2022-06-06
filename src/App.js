6759b8f8e2b6b581547839c841d426dc9562b346
shadevil <linkinfan@bk.ru>
2022-06-06 16:41:22 +0500

    clear code, get post decomposition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import PostService from "./API/PostService";
 import { usePosts } from "./components/hooks/usePosts";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
@@ -9,20 +10,25 @@ import MyModal from "./components/UI/MyModal/MyModal";
 import "./styles/App.css";
 
 function App() {
-  const [posts, setPosts] = useState([ ]);
-
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort,filter.query);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
+  useEffect(() => {
+    fetchPosts();
+  }, []);
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
 
-  async function fetchPosts(){
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    setPosts(response.data);
+  async function fetchPosts() {
+    setIsPostsLoading(true);
+    const posts = await PostService.getAll();
+    setPosts(posts);
+    setIsPostsLoading(false);
   }
   //Get post from child component
   const removePost = (post) => {
@@ -31,25 +37,24 @@ function App() {
 
   return (
     <div className="App">
-      <MyButton style={{marginRight: 10}} onClick={fetchPosts}>GET POSTS</MyButton>
-      <MyButton style={{marginTop: 20}} onClick={() => setModal(true)}>
+      <MyButton style={{ marginRight: 10 }} onClick={fetchPosts}>
+        GET POSTS
+      </MyButton>
+      <MyButton style={{ marginTop: 20 }} onClick={() => setModal(true)}>
         Создать пользователя
       </MyButton>
-      <MyModal visible={modal} setVisible={setModal} >
+      <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
 
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      {sortedAndSearchedPosts.length ? (
-        <PostList
-          remove={removePost}
-          posts={sortedAndSearchedPosts}
-          title="Посты про js"
-        />
-      ) : (
-        <h1 style={{ textAlign: "center" }}>Посты не найдены</h1>
-      )}
+
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title="Посты про js"
+      />
     </div>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,6 +3,11 @@ import { CSSTransition } from "react-transition-group";
 import { TransitionGroup } from "react-transition-group";
 import PostItem from "./PostItem";
 const PostList = ({posts, title, remove}) => {
+  if(!posts.length){
+    return(
+      <h1 style={{ textAlign: "center" }}>Посты не найдены</h1>
+    )
+  }
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>{title}</h1>
